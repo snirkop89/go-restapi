a212d8f78f4e7bca7a1becf44d7ec2c04a71dd8b
educative <educative@educative.io>
2022-05-11 22:10:40 +0300

    Add connection to db

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"time"
+
+	"github.com/snirkop89/go-restapi/internal/db"
 )
 
 // Run - is responsible for the instantiation and startup
 // of our go application
 func Run() error {
 	fmt.Println("starting our application")
+
+	var dbConn *db.Database
+	var err error
+	// give a few seconds for the postgres docker to start
+	for i := 0; i < 2; i++ {
+		dbConn, err = db.NewDatabase()
+		if err != nil {
+			log.Println("Failed to connect to the database")
+		}
+		time.Sleep(2 * time.Second)
+	}
+	if err != nil {
+		return err
+	}
+	if err := dbConn.Ping(context.Background()); err != nil {
+		log.Println(err)
+	}
+
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/snirkop89/go-restapi
 
 go 1.18
+
+require (
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.10.5
+)
